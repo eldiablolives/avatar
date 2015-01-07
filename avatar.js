5e0cbedb65d3e91a68486ddabe5b601ee41924e8
el'diablo <eldiablolives@live.com>
2015-01-07 15:53:09 +0700

    initial load

--- avatar.js
+++ avatar.js
@@ -1,19 +1,30 @@
 function Avatar(elementId, params) {
     var self = this;
-    if(!params)
+    self.id = elementId;
+
+    if (!params)
         params = {};
 
     self.av = document.getElementById(elementId);
 
-    self.offsetX = params.offsetX || params.border * -1 || 0;
-    self.offsetY = params.offsetY || params.border * -1 || 0;
-    self.offsetW = params.offsetW || params.border || 0;
-    self.offsetH = params.offsetH || params.border || 0;
+    this.conf = {
+        offsetX: self.av.getAttribute('offset-x') || self.av.getAttribute('border') * -1 || params.offsetX || params.border * -1 || 0,
+        offsetY: self.av.getAttribute('offset-y') || self.av.getAttribute('border') * -1 || params.offsetY || params.border * -1 || 0,
+        offsetW: self.av.getAttribute('offset-width') || self.av.getAttribute('border') || params.offsetW || params.border || 0,
+        offsetH: self.av.getAttribute('offset-height') || self.av.getAttribute('border') || params.offsetH || params.border || 0,
+        target: self.av.getAttribute('target') || params.target
+    };
 
-    self.bgx = self.offsetX;
-    self.bgy = self.offsetY;
+    self.bgx = self.conf.offsetX;
+    self.bgy = self.conf.offsetY;
     self.baseBgx = self.bgx;
     self.baseBgy = self.bgy;
+
+    this.xconf = {
+        offsetX: self.av.getAttribute('offset-x') || self.av.getAttribute('border') * -1 || params.offsetX || params.border * -1 || 0
+    };
+
+    var start;
 
     self.av.addEventListener('dragenter', function (e) {
         self.av.classList.add('avatar-over');
@@ -86,17 +97,17 @@ function Avatar(elementId, params) {
                 diffy = (e.clientY - start.y);
             }
 
-            if (self.bgx >= self.offsetX && diffx > 0)
-                self.bgx = self.offsetX;
-            else if (self.bgx + self.pw + diffx <= self.aw + self.offsetX)
-                self.bgx = (self.pw - self.aw) * -1 + self.offsetX;
+            if (self.bgx >= self.conf.offsetX && diffx > 0)
+                self.bgx = self.conf.offsetX;
+            else if (self.bgx + self.pw + diffx <= self.aw + self.conf.offsetX)
+                self.bgx = (self.pw - self.aw) * -1 + self.conf.offsetX;
             else
                 self.bgx += diffx;
 
-            if (self.bgy >= self.offsetY && diffy > 0)
-                self.bgy = self.offsetY;
-            else if (self.bgy + self.ph + diffy <= self.ah + self.offsetY)
-                self.bgy = (self.ph - self.ah) * -1 + self.offsetY;
+            if (self.bgy >= self.conf.offsetY && diffy > 0)
+                self.bgy = self.conf.offsetY;
+            else if (self.bgy + self.ph + diffy <= self.ah + self.conf.offsetY)
+                self.bgy = (self.ph - self.ah) * -1 + self.conf.offsetY;
             else
                 self.bgy += diffy;
 
@@ -108,28 +119,19 @@ function Avatar(elementId, params) {
             self.baseBgx = self.bgx / zoom;
             self.baseBgy = self.bgy / zoom;
 
-            self.av.style.backgroundPosition = self.bgx + ' ' + self.bgy;
+            self.av.style.backgroundPosition = self.bgx + 'px ' + self.bgy + 'px';
         }
     });
 }
 
 Avatar.prototype = {
+    handlers: {},
     crop: function (url, callback) {
         var self = this;
-        if (!callback) {
-            callback = url;
-            url = null;
-        }
-
         var canvas = document.createElement('canvas');
 
         var bgx = self.bgx;
         var bgy = self.bgy;
-
-        //var offsetX = self.params.offsetX || self.params.border || 0;
-        //var offsetY = self.params.offsetY || self.params.border || 0;
-        //var offsetW = self.params.offsetW || self.params.border || 0;
-        //var offsetH = self.params.offsetH || self.params.border || 0;
 
         if (bgx < 0)
             bgx = bgx * -1;
@@ -138,17 +140,19 @@ Avatar.prototype = {
 
         var sx = bgx * self.ratio;
         var sy = bgy * self.ratio;
-        var sw = (self.aw - self.offsetW - self.offsetX) * self.ratio;
-        var sh = (self.ah - self.offsetH - self.offsetY) * self.ratio;
+        var sw = (self.aw - self.conf.offsetW - self.conf.offsetX) * self.ratio;
+        var sh = (self.ah - self.conf.offsetH - self.conf.offsetY) * self.ratio;
 
         canvas.width = sw;
         canvas.height = sh;
         var ctx = canvas.getContext('2d');
         ctx.drawImage(this.img, sx, sy, sw, sh, 0, 0, sw, sh);
-        var imgdata = ctx.canvas.toDataURL('image/png');
+        var imgdata = ctx.canvas.toDataURL('image/jpg');
+        self.signal('cropped', imgdata);
 
-        if (url) {
+        if (url || this.conf.target) {
             var xhr = new XMLHttpRequest();
+            var hxrResponse;
             var fd = new FormData();
 
             var data = atob(imgdata.split(',')[1]);
@@ -158,24 +162,38 @@ Avatar.prototype = {
                 bytes[i] = data.charCodeAt(i);
             }
 
-            fd.append('file', new Blob([bytes],{type: imgdata.split(':')[1].split(';')[0]}));
+            fd.append('file', new Blob([bytes], {type: imgdata.split(':')[1].split(';')[0]}));
 
             xhr.addEventListener('load', function () {
-                console.log('done!!!!');
+                self.signal('uploaded', hxrResponse);
+                if(callback)
+                    callback(null, hxrResponse);
             });
             xhr.addEventListener('error', function (err) {
-                console.log('ERROR');
+                self.signal('error', err)
+                if(callback)
+                    callback(err);
             });
 
-            xhr.open('post', 'http://localhost:8081/upload/ogdZ0Omv27PLA8yA', false);
+            self.signal('uploading', imgdata);
+            xhr.onreadystatechange = function () {
+                if (this.readyState !== 4) return;
+                if (this.status === 200) {
+                    if (xhr.readyState === 4) {
+                        hxrResponse = xhr.response;
+                    }
+
+                } else {
+                    self.signal('error', 'Error uploading file');
+                }
+            };
+            xhr.open('post', url || this.conf.target, true);
             xhr.send(fd);
         }
-
-        if(!url && callback)
+        else if (callback)
             callback(null, imgdata);
 
         return imgdata;
-
     },
     paint: function (img, zoom) {
         var self = this;
@@ -220,5 +238,26 @@ Avatar.prototype = {
 
         self.av.style.backgroundSize = self.pw + 'px ' + self.ph + 'px';
         self.av.style.backgroundPosition = self.bgx + ' ' + self.bgy;
+    },
+    on: function (event, handler) {
+        if (!event)
+            event = 'all';
+
+        if (!this.handlers[event])
+            this.handlers[event] = [];
+
+        this.handlers[event].push(handler);
+    },
+    signal: function (event, err, data) {
+        if (event && this.handlers[event]) {
+            for (var i = 0; i < this.handlers[event].length; i++) {
+                this.handlers[event][i].apply(this,[err, data]);
+            }
+        }
+
+        if (this.handlers.all) {
+            for (var i = 0; i < this.handlers.all.length; i++)
+                this.handlers.all[i](err, data);
+        }
     }
 };
